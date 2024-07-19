416d2cbe18f7978ef748517b0d7b4e0081622052
PaulMainboard <paulwilliams669@gmail.com>
2024-07-18 21:20:06 -0400

    Created and Computer class and the converBinaryToBoolean function. Also add some more documentation.

--- program.py
+++ program.py
@@ -52,7 +52,9 @@
     
 '''
 
+
 '''
+TODO:
     Section---Research: 
     1. Ohm's Law
     2. Trig limits
@@ -60,23 +62,89 @@
     4. Logic gates
     5. Temperature equations
     6. The science of black holes
+    7. Bitwise Operators
 '''
 
+###################### Functions ########################
+def convertBinaryToBoolean(bits: int) -> list:    
+    """
+    Convert a binary literal into a boolean list
 
+    Args:
+        bits (int): Any binary literal
+
+    Returns:
+        list: Boolean
+    """
+    
+    # Convert the binary literal to a binary string and remove the '0b'
+    binary_string = bin(bits)[2:]
+    
+    # Convert each bit to a boolean and create the boolean list
+    boolean_list = [bit == '1' for bit in binary_string]
+    
+    return boolean_list
+
+#TODO: Create a convert boolean list into binary function
 ####################### Classes #########################
 
 
 
-class node:
+class Node_Double:
+    """
+    A basic structure for all the objects in this World.
+    This stucture has double inputs [One In, One Out].
+    
+    Attributes
+    ----------
+    dataValues : Any(String, Array, Integer, Boolean, Double)
+    next : node
+    previous : node
+    
+    Methods
+    -------
+    __str__() -> String :
+        Return the data values within of the node.
+    """
     def __init__(self, dataValues=None):
         self.dataValues = dataValues
         self.previous = None
         self.next = None
         
+    def __str__(self) -> str:
+        """_summary_
+           Return all properties of the node class
+        Returns:
+            str: dataValues, previous, next
+        """
+        return f'{self.dataValues}, {self.previous}, {self.next}'
         
-e1 = node('Mon')
-e2 = node('Wed')
-e3 = node('Tue')
+
+#TODO: Make Single Input Node Class
+#TODO: Make Single Output Node Class
+
+class Computer:
+    
+    """
+    Create a computer based off of for elements, 
+    A CPU, a set amount of RAM, a set amount of storage,
+    and a operating system.
+    
+    TODO:
+    Explain and break down how to computer when function,
+    input, and output Data [Binary]
+    """
+    
+    def __init__(self, cpu, ram, storage, os):
+        self.cpu = cpu 
+        self.ram = ram
+        self.storage = storage
+        self.os = os
+
+################### Test Program ##################        
+e1 = Node_Double('Mon')
+e2 = Node_Double('Wed')
+e3 = Node_Double('Tue')
 
 e1.next = e3
 e3.previous = e1
